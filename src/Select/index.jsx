9bce7e5539c65cf63604a9bab4564ff7d5eff7f8
Morgan Carter <morgancarter1@gmail.com>
2019-02-19 16:40:04 +1100

    style Select and MultiSelect to fit in with TextFields

--- src/Select/index.jsx
+++ src/Select/index.jsx
@@ -50,6 +50,96 @@ class Select extends React.Component<Props, State> {
 
     const id = `Select${count}`
 
+    const customStyles = {
+      control: (provided, state) => ({
+        ...provided,
+        width: '100%',
+        borderColor: state.isFocused ? '#2E64D9' : '#C7CBD6',
+        boxShadow: state.isFocused ? `0 0 0 2px rgba(46, 100, 217, 0.25)` : null,
+        minWidth: 120,
+        minHeight: 34,
+        '&:hover': {
+          borderColor: state.isFocused ? '#2E64D9' : '#C7CBD6',
+        },
+      }),
+      valueContainer: (provided) => ({
+        ...provided,
+        paddingTop: 2,
+        paddingBottom: 2,
+        paddingLeft: 2,
+        paddingRight: 2,
+      }),
+      placeholder: (provided) => ({
+        ...provided,
+        color: '#8F9BB3',
+        marginLeft: 10,
+      }),
+      singleValue: (provided) => ({
+        ...provided,
+        color: 'inherit',
+        marginLeft: 10,
+      }),
+      multiValue: (provided) => ({
+        ...provided,
+        background: '#D5D9E0',
+        borderRadius: 3,
+      }),
+      multiValueLabel: (provided) => ({
+        ...provided,
+        color: 'inherit',
+        fontSize: 14,
+        lineHeight: '24px',
+        paddingTop: 0,
+        paddingLeft: 8,
+        paddingRight: 4,
+        paddingBottom: 0,
+      }),
+      multiValueRemove: (provided) => ({
+        ...provided,
+        borderRadius: 3,
+        borderTopLeftRadius: 0,
+        borderBottomLeftRadius: 0,
+        ':hover': {
+          backgroundColor: 'transparent',
+          color: '#D63921',
+        },
+      }),
+      indicatorSeparator: (provided) => ({
+        ...provided,
+        background: '#C7CBD6',
+      }),
+      dropdownIndicator: (provided) => ({
+        ...provided,
+        padding: 4,
+        paddingTop: 6,
+        paddingBottom: 6,
+        color: '#6a7694',
+        ':hover': {
+          color: 'inherit',
+        },
+      }),
+      clearIndicator: (provided) => ({
+        ...provided,
+        padding: 4,
+        paddingTop: 6,
+        paddingBottom: 6,
+        color: '#6a7694',
+        ':hover': {
+          color: '#D63921',
+        },
+      }),
+      menu: (provided) => ({
+        ...provided,
+        boxShadow: '0 0 0 1px rgba(10, 24, 54, 0.06), 0 2px 16px rgba(10, 24, 54, 0.1)',
+        zIndex: 400,
+      }),
+      option: (provided, state) => ({
+        ...provided,
+        color: state.isSelected || state.isFocused ? 'white' : 'inherit',
+        background: state.isSelected || state.isFocused ? '#2E64D9' : 'transparent'
+      }),
+    }
+
     return (
       <div>
         { label ? (
@@ -61,6 +151,7 @@ class Select extends React.Component<Props, State> {
           </label>
         ) : null }
         <ReactSelect
+          styles={customStyles}
           className={styles.Select}
           isMulti={isMulti}
           isSearchable={isSearchable}
