61d6eb6b8654ba11d4b9b57cc1b667a445634782
Morgan Carter <morgancarter1@gmail.com>
2019-02-27 16:05:37 +1100

    add Label component

---
+++ src/Label/index.jsx
@@ -0,0 +1,34 @@
+import * as React from 'react'
+
+import * as styles from './Label.module.scss'
+
+type Props = {
+  /** A unique identifier for the label */
+  fieldID: string;
+  /** Text for the label */
+  label: string;
+  /** Is an optional field */
+  optional?: boolean;
+}
+
+function Label(props: Props) {
+  const {
+    fieldID,
+    label,
+    optional,
+  } = props
+
+  const optionalMarkup = optional ? <span className={styles.Optional}>(Optional)</span> : null;
+
+  return (
+    <label htmlFor={fieldID} className={styles.Label}>
+      {label} {optionalMarkup}
+    </label>
+  )
+}
+
+Label.defaultProps = {
+  optional: false
+}
+
+export default Label
