ddf80d9231532bd7c340afcef610ad01a74713f2
Morgan Carter <morgancarter1@gmail.com>
2019-02-28 10:30:03 +1100

    continue adding icons

--- src/icons/formattedIcons.js
+++ src/icons/formattedIcons.js
@@ -1,8 +1,5 @@
 import {
-  activityIcon as activity,
   alertIcon as alert,
-  areaIcon as area,
-  areaGrossIcon as areaGross,
   arrowLeftIcon as arrowLeft,
   arrowRightIcon as arrowRight,
   breakOptionIcon as breakOption,
@@ -10,12 +7,7 @@ import {
   cancelIcon as cancel,
   checkmarkIcon as checkmark,
   checkmarkLargeIcon as checkmarkLarge,
-  commentIcon as comment,
-  dateEndIcon as dateEnd,
-  dateStartIcon as dateStart,
   embedIcon as embed,
-  miscIcon as misc,
-  propertyIcon as property,
   warningCircleIcon as warningCircle,
   warningIcon as warning,
   warningTriangleIcon as warningTriangle,
@@ -24,25 +16,22 @@ import {
 
 export { date } from './icon/date';
 export { dateStart } from './icon/date-start';
-
-export const activityIcon = {
-  viewBox: '0 0 20 20',
-  body: activity,
-}
+export { dateEnd } from './icon/date-end';
+export { dateAlert } from './icon/date-alert';
+export { city } from './icon/city';
+export { property } from './icon/property';
+export { location } from './icon/location';
+export { activity } from './icon/activity';
+export { size } from './icon/size';
+export { areaGross } from './icon/area-gross';
+export { areaSales } from './icon/area-sales'
+export { decision } from './icon/decision';
+export { misc } from './icon/misc';
+export { comment } from './icon/comment';
 
 export const alertIcon = {
   viewBox: '0 0 20 20',
   body: alert,
-}
-
-export const areaIcon = {
-  viewBox: '0 0 20 20',
-  body: area,
-}
-
-export const areaGrossIcon = {
-  viewBox: '0 0 20 20',
-  body: areaGross,
 }
 
 export const arrowLeftIcon = {
@@ -80,34 +69,9 @@ export const checkmarkLargeIcon = {
   body: checkmarkLarge,
 }
 
-export const commentIcon = {
-  viewBox: '0 0 20 20',
-  body: comment,
-}
-
-export const dateEndIcon = {
-  viewBox: '0 0 20 20',
-  body: dateEnd,
-}
-
-export const dateStartIcon = {
-  viewBox: '0 0 20 20',
-  body: dateStart,
-}
-
 export const embedIcon = {
   viewBox: '0 0 20 20',
   body: embed,
-}
-
-export const miscIcon = {
-  viewBox: '0 0 20 20',
-  body: misc,
-}
-
-export const propertyIcon = {
-  viewBox: '0 0 20 20',
-  body: property,
 }
 
 export const warningIcon = {
--- stories/components/Icon.stories.js
+++ stories/components/Icon.stories.js
@@ -6,18 +6,31 @@ import { withKnobs, select } from '@storybook/addon-knobs';
 import Icon from 'Icon'
 
 const icons = {
-  Date: 'date',
-  DateStart: 'dateStart'
+  date: 'date',
+  dateStart: 'dateStart',
+  dateEnd: 'dateEnd',
+  dateAlert: 'dateAlert',
+  city: 'city',
+  property: 'property',
+  location: 'location',
+  activity: 'activity',
+  size: 'size',
+  areaGross: 'areaGross',
+  areaSales: 'areaSales',
+  decision: 'decision',
+  misc: 'misc',
+  comment: 'comment',
 }
+const defaultIcon = 'dateAlert'
 
 storiesOf('components/Icon', module)
   .addDecorator(withKnobs)
   .add('Icon', () => (
     <div style={{ background: "white", padding: 20 }}>
-      <Icon source={select('Icon Source', icons, 'date')} />
-      <Icon source={select('Icon Source', icons, 'date')} backdrop />
-      <Icon source={select('Icon Source', icons, 'date')} color="green" />
-      <Icon source={select('Icon Source', icons, 'date')} color="green" backdrop />
+      <Icon source={select('Icon Source', icons, defaultIcon)} />
+      <Icon source={select('Icon Source', icons, defaultIcon)} backdrop />
+      <Icon source={select('Icon Source', icons, defaultIcon)} color="green" />
+      <Icon source={select('Icon Source', icons, defaultIcon)} color="green" backdrop />
     </div>
   ))
           
